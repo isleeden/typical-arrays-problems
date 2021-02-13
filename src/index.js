7827f15ea2e1f0713c687e9b20a1e87830a9dd2c
isleeden <nkoreanarmy@gmail.com>
2021-02-13 11:19:48 +0500

    feat: implement min max avg

--- src/index.js
+++ src/index.js
@@ -1,12 +1,43 @@
+exports.min = function min(array) {
+    if (!array || array.length === 0) {
+        return 0;
+    }
 
-exports.min = function min (array) {
-  return 0;
-}
+    let minValue = array[0];
 
-exports.max = function max (array) {
-  return 0;
-}
+    array.forEach((item) => {
+        if (minValue > item) {
+            minValue = item;
+        }
+    });
 
-exports.avg = function avg (array) {
-  return 0;
-}
+    return minValue;
+};
+
+exports.max = function max(array) {
+    if (!array || array.length === 0) {
+        return 0;
+    }
+
+    let maxValue = array[0];
+
+    array.forEach((item) => {
+        if (maxValue < item) {
+            maxValue = item;
+        }
+    });
+
+    return maxValue;
+};
+
+exports.avg = function avg(array) {
+    if (!array || array.length === 0) {
+        return 0;
+    }
+
+    return (
+        array.reduce((acc, item) => {
+            return acc + item;
+        }) / array.length
+    );
+};
